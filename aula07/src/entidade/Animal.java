959f0ac908b3bc191338b5d91026a5ba6a72b17a
Eduardo josé <nascimentodasilvae887@gmail.com>
2024-09-05 13:33:24 -0300

    Add files via upload

---
+++ aula07/src/entidade/Animal.java
@@ -0,0 +1,9 @@
+package entidade;
+
+public class Animal {
+	
+	
+	 public String emitirSom() {
+	        return "O som do ";
+	    }
+}
---
+++ aula07/src/principal/Main.java
@@ -0,0 +1,39 @@
+package principal;
+
+import entidade.Animal;
+import entidade.Cachorro;
+import entidade.Gato;
+import entidade.vaca;
+
+public class Main {
+	  public static void main(String[] args) {
+	       
+	       Animal x = new Cachorro();
+	       
+		   System.out.println("");
+		   System.out.println(x.emitirSom());
+		   
+		   
+		   Animal y = new Gato();
+		   
+		   System.out.println("");
+		   System.out.println(y.emitirSom());
+		  
+		  Animal w = new vaca();
+		  
+		  
+		  System.out.println("");
+		   System.out.println(w.emitirSom());
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+	    }
+
+}
